9fa182db2179168aa0262dc294cee36e77b3cdf1
Shinnosuke Sawada <6warashi9@gmail.com>
2022-04-23 10:24:23 +0900

    feat(slices): sortされたコピーを返す実装を追加

--- slice/slice.go
+++ slice/slice.go
@@ -1,6 +1,10 @@
 package slice
 
-import "github.com/Warashi/functional-go/monad"
+import (
+	"github.com/Warashi/functional-go/monad"
+	"golang.org/x/exp/constraints"
+	"golang.org/x/exp/slices"
+)
 
 func Map[F, T any](from []F, f func(F) T) []T {
 	return monad.Map[[]T](MonadImpl[F, T]{}, from, f)
@@ -13,4 +17,10 @@ func Filter[T any](from []T, f func(T) bool) []T {
 }
 func ForEach[T any](from []T, f func(T)) {
 	monad.Do[struct{}, []struct{}](MonadImpl[T, struct{}]{}, from, f)
+}
+func Sorted[T constraints.Ordered](s []T) []T {
+	r := make([]T, len(s))
+	copy(r, s)
+	slices.Sort(r)
+	return r
 }
