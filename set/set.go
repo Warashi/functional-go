f5eae5dfeefa052cba2054b974c46b7b2b72ca8c
Shinnosuke Sawada <6warashi9@gmail.com>
2022-04-23 10:23:49 +0900

    feat(set): sliceへの変換を実装

--- set/set.go
+++ set/set.go
@@ -1,6 +1,10 @@
 package set
 
-import "github.com/Warashi/functional-go/monad"
+import (
+	"github.com/Warashi/functional-go/monad"
+	"golang.org/x/exp/constraints"
+	"golang.org/x/exp/slices"
+)
 
 type Set[T comparable] map[T]struct{}
 
@@ -31,4 +35,21 @@ func Filter[T comparable](from Set[T], f func(T) bool) Set[T] {
 }
 func ForEach[T comparable](from Set[T], f func(T)) {
 	monad.Do[struct{}, Set[struct{}]](MonadImpl[T, struct{}]{}, from, f)
+}
+func AsSlice[T comparable](from Set[T]) []T {
+	s := make([]T, 0, len(from))
+	ForEach(from, func(t T) {
+		s = append(s, t)
+	})
+	return s
+}
+func AsSliceSorted[T constraints.Ordered](from Set[T]) []T {
+	s := AsSlice(from)
+	slices.Sort(s)
+	return s
+}
+func AsSliceSortedFunc[T comparable](from Set[T], less func(a, b T) bool) []T {
+	s := AsSlice(from)
+	slices.SortFunc(s, less)
+	return s
 }
